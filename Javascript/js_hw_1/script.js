ec8885ac865ad85a9f7d4d85275845b5bec8dd82
Дюша <58334576+Menxy@users.noreply.github.com>
2023-01-31 23:16:04 +0200

    JS_HW_1: Final

---
+++ Javascript/js_hw_1/script.js
@@ -0,0 +1,29 @@
+/* 1 */
+let hello = 'hello'; console.log(hello);
+let owu = 'owu'; console.log(owu);
+let com = 'com'; console.log(com);
+let ua = 'ua'; console.log(ua);
+let one = 1; console.log(one);
+let ten = 10; console.log(ten);
+let nines = -999; console.log(nines);
+let pass = 123; console.log(pass);
+let pi = 3.14; console.log(pi);
+let two = 2.7; console.log(two);
+let steen = 16; console.log(steen);
+let plus = true; console.log(plus);
+let no = false; console.log(no);
+/* 2 */
+let firstName = 'Левчук';
+let middleName = 'Андрій';
+let lastName = 'Ярославович';
+let person = firstName + ' ' + middleName + ' ' + lastName
+console.log(person)
+/* 3 */
+let a = 100; console.log(typeof a);
+let b = '100'; console.log(typeof b);
+let c = true; console.log(typeof c);
+/* bonus */
+let name = prompt("Ім'я")
+let surname = prompt("Прізвище")
+let age = prompt("Вік")
+console.log(name + ' ' + surname + '.' + ' Вік: ' + age + ' років.')
