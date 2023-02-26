b4faac9f21bab11badb67f04a46dce45dd0e2877
Дюша <58334576+Menxy@users.noreply.github.com>
2023-02-26 16:07:55 +0200

    JS_HW_8: Cindy

--- Javascript/js_hw_8/script.js
+++ Javascript/js_hw_8/script.js
@@ -1,13 +1,14 @@
 // - Створити функцію конструктор для об'єктів User з полями id, name, surname , email, phone
-function User(id, name, surname , email, phone){
+/*function User(id, name, surname , email, phone){
     this.id = id;
     this.name = name;
     this.surname = surname;
     this.email = email;
     this.phone = phone
-}
+}*/
+
 // створити пустий масив, наповнити його 10 об'єктами new User(....)
-let user1 = new User(1,'Jeka', 'Sich', 'jekasich@gmail.com', '0667775554');
+/*let user1 = new User(1,'Jeka', 'Sich', 'jekasich@gmail.com', '0667775554');
 let user2 = new User(2,'Jane', 'Doe', 'janedoe@gmail.com', '0636675554');
 let user3 = new User(3,'Any', 'Long', 'anylong@gmail.com', '0667788854');
 let user4 = new User(4,'Vira', 'Tar', 'viratar@gmail.com', '0696545554');
@@ -18,8 +19,9 @@ let user8 = new User(8,'Jenny', 'Bald', 'jennybald@gmail.com', '0668453624');
 let user9 = new User(9,'Peter', 'Young', 'peteryoung@gmail.com', '0965347554');
 let user10 = new User(10,'Walter', 'Benz', 'walterbenz@gmail.com', '0661997544');
 
-let usersArray = [user1, user2, user3,user4,user5,user6,user7,user8,user9,user10,];
+let usersArray = [user1, user2, user3,user4,user5,user6,user7,user8,user9,user10,];*/
 /*console.log(usersArray);*/
+
 //
 // - Взяти масив з  User[] з попереднього завдання, та відфільтрувати , залишивши тільки об'єкти з парними id (filter)
 /*let evenArray = usersArray.filter(obj => obj.id % 2 === 0);
@@ -28,7 +30,7 @@ console.log(evenArray);*/
 /*let highSort = usersArray.sort((id1, id2) => id2 - id1);
 console.log(highSort);*/
 // - створити класс для об'єктів Client з полями id, name, surname , email, phone, order (поле є масивом зі списком товарів)
-class Client {
+/*class Client {
     constructor(id, name, surname , email, phone, order) {
         this.id = id;
         this.name = name;
@@ -37,9 +39,9 @@ class Client {
         this.phone = phone;
         this.order = order;
     }
-}
+}*/
 // створити пустий масив, наповнити його 10 об'єктами Client
-let clientsArray = []
+/*let clientsArray = []
 for (const item of usersArray) {
     clientsArray[item.id - 1] = new Client(item.id, item.name, item.surname, item.email, item.phone,[])
 }
@@ -52,12 +54,12 @@ clientsArray[5]['order'] = ['katana', 'sheet'];
 clientsArray[6]['order'] = ['paper', 'pen', 'pencil', 'fork'];
 clientsArray[7]['order'] = ['gin'];
 clientsArray[8]['order'] = ['vinegar', 'cabages', 'cucumbers', 'onion'];
-clientsArray[9]['order'] = ['water', 'vine', 'juice'];
+clientsArray[9]['order'] = ['water', 'vine', 'juice'];*/
 
 /*console.log(clientsArray);*/
 // - Взяти масив (Client [] з попереднього завдання).Відсортувати його по кількості товарів в полі order по зростанню. (sort)
 
-clientsArray.sort((f,s) => f['order'].length - s['order'].length)
+/*clientsArray.sort((f,s) => f['order'].length - s['order'].length)*/
 // console.log(clientsArray);
 
 // - Створити функцію конструктор яка дозволяє створювати об'єкти car, з властивостями модель,
@@ -68,7 +70,7 @@ clientsArray.sort((f,s) => f['order'].length - s['order'].length)
 // -- changeYear (newValue) - змінює рік випуску на значення newValue
 // -- addDriver (driver) - приймає об'єкт який "водій" з довільним набором полів, і додає його в поточний об'єкт car
 
-function CarCreator(model, manufacturer, year, maxSpeed, engine) {
+/*function CarCreator(model, manufacturer, year, maxSpeed, engine) {
     this.model = model;
     this.manufacturer = manufacturer;
     this.year = year;
@@ -92,7 +94,7 @@ function CarCreator(model, manufacturer, year, maxSpeed, engine) {
         this.driver = driver
     }
 
-}
+}*/
 // let car1 = new CarCreator('Ford Taurus', 'Ford', 2022, 200, 2500)
 
 
@@ -105,6 +107,7 @@ function CarCreator(model, manufacturer, year, maxSpeed, engine) {
 // -- changeYear (newValue) - змінює рік випуску на значення newValue
 // -- addDriver (driver) - приймає об'єкт який "водій" з довільним набором полів, і додає його в поточний об'єкт car
 
+/*
 class Car {
     constructor(model, manufacturer, year, maxSpeed, engine) {
         this.model = model;
@@ -134,10 +137,46 @@ class Car {
 
 let car2 = new Car('Ford Taurus', 'Ford', 2022, 200, 2500)
 
+*/
 
 // -створити класс/функцію конструктор попелюшка з полями ім'я, вік, розмір ноги.
 // Створити масив з 10 попелюшок.
+/*function Cinderella(name, age, footSize) {
+    this.name = name;
+    this.age = age;
+    this.footSize = footSize;
+}
+let cindy1 = new Cinderella('Ana', 18, 36);
+let cindy2 = new Cinderella('Cindy', 20, 35);
+let cindy3 = new Cinderella('Lola', 23, 38);
+let cindy4 = new Cinderella('Lena', 17, 32);
+let cindy5 = new Cinderella('Carter', 19, 33);
+let cindy6 = new Cinderella('Tiffany', 18, 40);
+let cindy7 = new Cinderella('Dolores', 25, 39);
+let cindy8 = new Cinderella('Rein', 17, 35);
+let cindy9 = new Cinderella('Jessica', 19, 36);
+let cindy10 = new Cinderella('Zoey', 22, 38);
+let cindyArray = [cindy1, cindy2, cindy3, cindy4, cindy5, cindy6, cindy7, cindy8, cindy9, cindy10]*/
 
 // Сторити об'єкт класу "принц" за допомоги класу який має поля ім'я, вік, туфелька яку він знайшов.
 //     За допомоги циклу знайти яка попелюшка повинна бути з принцом.
+/*class Prince {
+    constructor(name, age, shoe) {
+        this.name = name;
+        this.age = age;
+        this.shoe = shoe;
+    }
+}
+let prince = new Prince('Alfred', 25, 40)*/
+// console.log(prince);
+/*let winner;
+for (const cindy of cindyArray) {
+    if (cindy.footSize === prince.shoe) {
+        winner = cindy;
+        break
+    }
+}
+console.log(winner);*/
 //     Додатково, знайти необхідну попелюшку за допомоги функції масиву find та відповідного колбеку
+/*
+console.log(cindyArray.find(cindy => cindy.footSize === prince.shoe));*/
