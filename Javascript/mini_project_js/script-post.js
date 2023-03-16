b0966616268de84f08ea9894f10254f1203e198d
Дюша <58334576+Menxy@users.noreply.github.com>
2023-03-16 08:29:08 +0200

    JS_mini_project: Probably Final

---
+++ Javascript/mini_project_js/script-post.js
@@ -0,0 +1,49 @@
+/*
+    На странице post-details.html:
+    - Вивести всю, без виключення, інформацію про об'єкт post на який клікнули .
+    - Нижчє інформаці про пост, вивести всі коментарі поточного поста (ендпоінт
+    - https://jsonplaceholder.typicode.com/posts/POST_ID/comments)*/
+let postRepack = new URL(location.href);
+let postNum = JSON.parse(postRepack.searchParams.get('postContainer'))
+postUrl = new URL(`https://jsonplaceholder.typicode.com/posts/${postNum}`)
+commentsUrl = new URL(`https://jsonplaceholder.typicode.com/posts/${postNum}/comments`)
+fetch(postUrl)
+    .then(post => post.json())
+    .then(post => {
+        let header = document.createElement('h1');
+        header.classList.add('header');
+        header.innerText = `Post ID ${post.id}`;
+        let wrap = document.createElement('div');
+        wrap.classList.add('wrap');
+        for (key in post) {
+            let item = document.createElement('div');
+            item.classList.add('user-item');
+            item.innerText = `${key}: ${post[key]}.`
+            wrap.appendChild(item)
+        }
+        document.body.appendChild(header)
+        document.body.appendChild(wrap)
+    })
+fetch(commentsUrl)
+    .then(comments => comments.json())
+    .then(comments => {
+        let header = document.createElement('h3');
+        header.classList.add('header');
+        header.innerText = `Comments`;
+        let commentsList = document.createElement('div');
+        commentsList.classList.add('posts-list');
+        for (const comment of comments) {
+            let commentPost = document.createElement('div');
+            commentPost.classList.add('post-li')
+            for (const commentKey in comment) {
+                let commentLine = document.createElement('div');
+                commentLine.innerText = `${commentKey}: ${comment[commentKey]}`
+                commentPost.appendChild(commentLine)
+            }
+            commentsList.appendChild(commentPost)
+        }
+
+        document.body.appendChild(header)
+        document.body.appendChild(commentsList)
+    })
+
--- Javascript/mini_project_js/script-user.js
+++ Javascript/mini_project_js/script-user.js
@@ -1,11 +1,12 @@
-// На странице user-details.html:
-// 4 Вивести всю, без виключення, інформацію про об'єкт user на який клікнули
+/*
+    На странице user-details.html:
+    - Вивести всю, без виключення, інформацію про об'єкт user на який клікнули */
 
 let repack = new URL(location.href);
 let user = JSON.parse(repack.searchParams.get('container'));
 let header = document.createElement('h1');
 header.classList.add('header');
-header.innerText = user.username;
+header.innerText = `User "${user.username}"`;
 let wrap = document.createElement('div');
 wrap.classList.add('wrap');
 
@@ -22,17 +23,21 @@ for (key in user) {
         }
         item.appendChild(ulList)
     } else {
-        item.innerText = `${key}: ${user[key]}: `
+        item.innerText = `${key}: ${user[key]}.`
     }
     wrap.appendChild(item)
 }
 
 document.body.appendChild(wrap)
 document.body.appendChild(header)
-// 5 Додати кнопку "post of current user", при кліку на яку, з'являються title всіх постів
-// поточного юзера
-// (для получения постов используйте эндпоинт
-// https://jsonplaceholder.typicode.com/users/USER_ID/posts)
+
+/*
+    - Додати кнопку "post of current user", при кліку на яку, з'являються title всіх постів
+    поточного юзера (для получения постов используйте эндпоинт
+    https://jsonplaceholder.typicode.com/users/USER_ID/posts)
+    - Каждому посту додати кнопку/посилання, при кліку на яку відбувається перехід на
+    сторінку post-details.html, котра має детальну інфу про поточний пост.*/
+
 let postBtn = document.createElement('a');
 postBtn.href = '#';
 postBtn.classList.add('button', 'post-button');
@@ -46,20 +51,22 @@ postBtn.addEventListener('click', function () {
     fetch(postsUrl)
         .then(posts => posts.json())
         .then(posts => {
-            let postsList = document.createElement('ul');
+            let postsList = document.createElement('div');
             postsList.classList.add('posts-list');
             postsList.id = 'posts-list';
             for (const post of posts) {
-                let postLi = document.createElement('li');
-                postLi.classList.add('post-li')
+                let postLi = document.createElement('div');
+                let postLiButton = document.createElement('a');
+                postLiButton.target = '_blanc';
+                postLiButton.href = `post-details.html?postContainer=` + JSON.stringify(post.id);
+                postLiButton.classList.add('post-li-button');
+                postLiButton.innerText = 'Details';
+                postLi.classList.add('post-li');
                 postLi.innerText = `${post.title}`;
+                postLi.appendChild(postLiButton);
                 postsList.appendChild(postLi)
             }
             document.body.appendChild(postsList)
         })
 })
 
-
-//     6 Каждому посту додати кнопку/посилання, при кліку на яку відбувається перехід на
-//     сторінку post-details.html, котра має детальну інфу про поточний пост.
-//
